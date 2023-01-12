5d4496b2fc89de859fcc4bf0ade3943d33a3121b
Advantage <advantage@192.168.1.8>
2023-01-12 17:26:29 +0200

    Initial commit

---
+++ MessengerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MessengerApp
+//
+//  Created by Advantage on 12/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
